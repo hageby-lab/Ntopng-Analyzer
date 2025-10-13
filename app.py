dbd74b90229bc7bf662e8623a4e55afe37c5f9c1
hageby-lab <hageby2014@gmail.com>
2025-10-13 22:29:18 +0300

    Изменена кодировка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException, Depends, BackgroundTasks
+﻿from fastapi import FastAPI, Request, HTTPException, Depends, BackgroundTasks
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
